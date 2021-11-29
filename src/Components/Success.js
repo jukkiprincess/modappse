b2fd203fc7bc48121adfb30b2042fd7af7ddcd03
Juki <jukidev@protonmail.com>
2021-11-29 21:16:32 +0100

    Update Success.js

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -5,8 +5,8 @@ class Success extends Component {
     render() {
         return (
             <Grid item>
-                <h1 style={{textAlign: "center", color: "#00e676"}}>Success! Your ban appeal has been submitted to the mods!</h1>
-                <h4>Please allow some time for them to review your appeal. Abusing this system will result in a perma-ban.</h4>
+                <h1 style={{textAlign: "center", color: "#00e676"}}>Success! Your mod app has been submitted to the reviewers!</h1>
+                <h4>Please allow some time for them to review your mod app. Abusing this system will result in a perma-ban.</h4>
             </Grid>
         );
     }
